dc7ee1b395b75ce6c800bae801eff5ea371981f4
jan.vanachte <jan.vanachte@javayum.net>
2021-05-08 16:06:27 +0200

    CodeBox component based on Bootstrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {StoreModule} from "@ngrx/store";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {environment} from "../environments/environment.prod";
 import {rootInitialState, rootMetaReducers, rootReducers} from "./state/app.reducer";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
@@ -27,6 +28,8 @@ import {rootInitialState, rootMetaReducers, rootReducers} from "./state/app.redu
                 initialState: rootInitialState
             }),
         EffectsModule.forRoot(),
+        NgbModule.forRoot(),
+
         StoreDevtoolsModule.instrument({
             maxAge: 25, // Retains last 25 states
             logOnly: environment.production, // Restrict extension to log-only mode
