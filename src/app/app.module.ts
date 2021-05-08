8e3c53e88e030e9789cc77bb16394c6ad04ba46d
jan.vanachte <jan.vanachte@javayum.net>
2021-05-08 17:14:54 +0200

    update to bootstrap5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ComponentsModule} from './shared/components/components.module';
-import {CodeModule} from './code/code.module';
-import {EffectsModule} from "@ngrx/effects";
-import {StoreModule} from "@ngrx/store";
-import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {environment} from "../environments/environment.prod";
-import {rootInitialState, rootMetaReducers, rootReducers} from "./state/app.reducer";
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentsModule } from './shared/components/components.module';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ComponentsModule,
-        CodeModule,
-        StoreModule.forRoot(
-            rootReducers,
-            {
-                metaReducers: rootMetaReducers,
-                initialState: rootInitialState
-            }),
-        EffectsModule.forRoot(),
-        NgbModule.forRoot(),
-
-        StoreDevtoolsModule.instrument({
-            maxAge: 25, // Retains last 25 states
-            logOnly: environment.production, // Restrict extension to log-only mode
-        })
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
