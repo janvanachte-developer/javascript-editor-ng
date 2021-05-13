f7511c3a8ca4f83c83ca4f293cd257e54b62644c
jan.vanachte <jan.vanachte@javayum.net>
2021-05-13 18:16:34 +0200

    Observable

--- src/app/code/containers/code-editing/code-editing.component.ts
+++ src/app/code/containers/code-editing/code-editing.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import CodeStoreService from "../../state/code-store.service";
-import Store, {UnsubscribeCallback} from "../../../state/model/store";
 import {CodeModel} from "../../state/model/code.model";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-code-editing',
@@ -9,66 +9,65 @@ import {CodeModel} from "../../state/model/code.model";
     styleUrls: ['./code-editing.component.css']
 })
 export class CodeEditingComponent implements OnInit, OnDestroy {
-    codeAsString: string;
-    _store: Store<CodeModel>;
-    private unsubscribeCallback: UnsubscribeCallback;
 
+    private unsubscribeCallback: Subscription;
+
+    code: CodeModel;
+
+    codeAsString: string;
     public tagsA: string[];
     public tagsB: string[];
 
-    constructor(service: CodeStoreService) {
-
-        this._store = service.getStore();
-        this.codeAsString = this._store.getState().codeAsString;
-
+    constructor(private _service: CodeStoreService) {
         this.tagsA = [ "awesome", "cool" ];
-        this.tagsB = [ "bad", "lazy" ];
     }
 
-    public addTagToB( newTagName: string ) : void {
+    public addTagToB(newTagName: string): void {
 
         console.log('adding ' + newTagName + ' to ' + this.tagsB);
         //this.tagsB = this.tagsB.concat( newTagName );
-        this._store.dispatch({type: CodeStoreService.ADD_TAG, payload: newTagName})
+        this._service.dispatch({type: CodeStoreService.ADD_TAG, payload: newTagName})
 
     }
 
 
     // I handle the "remove" event being emitted from the tags component for TagsB.
-    public removeFromB( index: number ) : void {
+    public removeFromB(index: number): void {
         console.log('removing ' + this.tagsB[index] + ' from ' + this.tagsB);
-        this._store.dispatch({type: CodeStoreService.REMOVE_TAG, payload: index})
+        this._service.dispatch({type: CodeStoreService.REMOVE_TAG, payload: index})
 
-/*
-        this.tagsB = [
-            ...this.tagsB.slice( 0, index ),
-            ...this.tagsB.slice( index + 1 )
-        ];
-*/
+        /*
+                this.tagsB = [
+                    ...this.tagsB.slice( 0, index ),
+                    ...this.tagsB.slice( index + 1 )
+                ];
+        */
 
     }
 
     ngOnInit(): void {
 
-        console.log('ngOnInit')
-        this.unsubscribeCallback = this._store.subscribe(() => {
+        console.log('ngOnInit');
+        this.unsubscribeCallback = this._service.getStore().subscribe((next) => {
             console.log('new data ')
-            this.codeAsString = this._store.getState().codeAsString;
-            this.tagsB = this._store.getState().tags;
+            this.code = next;
+            this.codeAsString = next.codeAsString;
+            this.tagsB = next.tags;
         })
     }
 
     updateCode(codeAsString: string) {
-        console.log('updateCode '  + codeAsString)
-        this._store.dispatch(
-            { type: CodeStoreService.SET_CODE,
-                    payload: codeAsString})
+        console.log('updateCode ' + codeAsString)
+        this._service.dispatch(
+            {
+                type: CodeStoreService.SET_CODE,
+                payload: codeAsString
+            })
 
     }
 
     ngOnDestroy(): void {
-        this.unsubscribeCallback
-        ();
+        this.unsubscribeCallback.unsubscribe();
     }
 
 
