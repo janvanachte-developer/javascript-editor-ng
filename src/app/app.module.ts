90d342df036f09fb69ab2ac2fbc2076f741317ab
jan.vanachte <jan.vanachte@javayum.net>
2021-05-30 18:33:57 +0200

    ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ComponentsModule } from './shared/components/components.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ComponentsModule} from './shared/components/components.module';
+import {CodeModule} from './code/code.module';
+import {EffectsModule} from "@ngrx/effects";
+import {StoreModule} from "@ngrx/store";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment.prod";
+import {rootInitialState, rootMetaReducers, rootReducers} from "./state/app.reducer";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ComponentsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ComponentsModule,
+        CodeModule,
+        StoreModule.forRoot(
+            rootReducers,
+            {
+                metaReducers: rootMetaReducers,
+                initialState: rootInitialState
+            }),
+        EffectsModule.forRoot(),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25, // Retains last 25 states
+            logOnly: environment.production, // Restrict extension to log-only mode
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
